1e5355043ef6a81fb4daf79eecc1add0599e8633
chung <buitrung200513@gmail.com>
2025-09-24 12:58:46 +0700

    bai tap 1

---
+++ src/main/java/com/codegym/model/User.java
@@ -0,0 +1,76 @@
+package com.codegym.model;
+
+import jakarta.validation.constraints.*;
+
+public class User {
+    @NotBlank(message = "First name is required")
+    @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
+    private String lastName;
+
+    @NotBlank(message = "Phone number is required")
+    @Pattern(regexp = "^\\d{10,15}$", message = "Phone number must be 10-15 digits")
+    private String phoneNumber;
+
+    @Min(value = 18, message = "Age must be at least 18")
+    @Max(value = 100, message = "Age must be less than or equal to 100")
+    private int age;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String phoneNumber, int age, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.age = age;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
